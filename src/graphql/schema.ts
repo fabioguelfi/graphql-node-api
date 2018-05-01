59e48f8fedcde02f71fe0b2a464323f2f0457906
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-01 00:27:13 -0300

    Implementando primeira Mutation

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -23,11 +23,23 @@ const typeDefs = `
   type Query {
     allUsers: [User]!
   }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
+
 `;
 
 const resolvers = {
   Query: {
     allUsers: () => Users
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const newUser = Object.assign({ id: Users.length + 1 }, args);
+      Users.push(newUser);
+      return newUser;
+    }
   }
 };
 
