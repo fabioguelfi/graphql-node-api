fd70ed55271e7a31e6330651f799223883d8d1a9
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-30 23:42:19 -0300

    Configurando o express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
-console.log('hello world');
-console.log('ok')
+import * as express from 'express';
+
+class App {
+
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middleware();
+  }
+
+  private middleware(): void {
+
+    this.express.use('/hello', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
+      res.send({
+        hello: 'hello world!'
+      })
+
+    });
+
+  }
+
+}
+
+export default new App().express;
